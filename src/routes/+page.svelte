f0860ebd52946e393091258bdd55f544e3d465f2
sleepyy-cat <hannach8@mit.edu>
2026-02-23 18:52:16 -0500

    up to step 3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<nav>
+    <a href="." class="current">Home</a>
+    <a href="projects">Projects</a>
+    <a href="contact">Contact</a>
+    <a href="resume">Resume</a>
+    <a href="https://github.com/sleepyy-cat" target="_blank">Github</a>
+</nav>
+<h1>Hanna Chen</h1>
+<p>
+    Hi everyone, I'm Hanna! I'm a senior at MIT double majoring in math and computer science. I've played piano and violin 
+    (mostly classical music) for more than a decade. I also really enjoy listening to music--classical, pop artists like 
+    Taylor Swift and Gracie Abrams, and just about anything else as well. In my free time, in addition to music, I also like 
+    hanging out with my friends and playing games like DnD and Celeste.
+</p>
+<img src="images/profilepic.jpg" alt="Picture of me in front of the Eiffel Tower" style="max-width: 30%; height: auto;">
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,26 @@
 <svelte:head>
   <title>Contact me</title>
 </svelte:head>
+<nav>
+    <a href="./">Home</a>
+    <a href="projects">Projects</a>
+    <a href="." class="current">Contact</a>
+    <a href="resume">Resume</a>
+    <a href="https://github.com/sleepyy-cat" target="_blank">Github</a>
+</nav>
+<h1>Contact</h1>
+<form action="mailto:hannach8@mit.edu" method="get">
+    <label>
+        Email: 
+        <input name="email" type="email"/>
+    </label>
+    <label>
+        Subject:
+        <input name="subject"/>
+    </label>
+    <label>
+        Message:
+        <textarea name="body"></textarea>
+    </label>
+    <button>Send</button>
+</form>
